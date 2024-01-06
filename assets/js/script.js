79425c6e647a5217472a8b63cf12594b92158cde
RowanKinross <rowankinross@yahoo.co.uk>
2024-01-06 20:31:40 +0000

    script-clean-up-and-comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-// Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-
-
-
 //variables for where to find the time-block <input> elements
 const hourNine = $('.hourNine');
 const hourTen = $('.hourTen');
@@ -14,7 +9,8 @@ const hourFifteen = $('.hourFifteen');
 const hourSixteen = $('.hourSixteen');
 const hourSeventeen = $('.hourSeventeen');
 
-//array to connect time-block <input> elements with their hour number value
+
+//array to connect time-block <input> elements with their hour's number value
 hoursArray = [
   [hourNine, '09', 'hourNine'],
   [hourTen, '10', 'hourTen'],
@@ -28,10 +24,11 @@ hoursArray = [
 ]
 
 
-const timeNow = dayjs().format('H')-8; //current time - juist the hour value
+const timeNow = dayjs().format('H'); //current time - just the hour value
 
 
-//The current day displayed the top of the calender when a user opens the planner
+
+//The current day is displayed the top of the calender when a user opens the planner
 $('#currentDay').text(dayjs().format('dddd, MMMM D')); //format e.g Saturday, January 6
 
 
@@ -49,50 +46,51 @@ for (let i=0; i<hoursArray.length; i++) { //for loop to remove the relevant css 
     hourArr[0].children('input').addClass('future')
   }
 
-
-
-
-// Persist events between refreshes of a page
+//Persist events between refreshes of a page
   //get saved items (from local storage) when you load the page
-function getSavedItems() {
-  const savedItems = JSON.parse(localStorage.getItem(hourArr[2]));
-  if (savedItems != null && savedItems != ``) {
-    //hourArr[0].children('input').removeAttr('placeholder');
-    hourArr[0].children('input').attr("value", savedItems);
-  } 
+  function getSavedItems() {
+    const savedItems = JSON.parse(localStorage.getItem(hourArr[2])); //the locally stored key will be equal to an iteration of hourArr[2] in this for loop 
+    if (savedItems != null && savedItems != ``) {
+      hourArr[0].children('input').attr("value", savedItems); // set locally stored text value in the input box
+    } 
+  }
+
+getSavedItems() //calls the function at the start of the page
 }
-getSavedItems()
-}
 
 
 
 
 
-//4. Allow a user to enter an event when they click a time block
-//5. Save the event in local storage when the save button is clicked in that time block.
+//Allow a user to enter an event when they click a time block
+  //Save the event in local storage when the save button is clicked in that time block.
 $( ".btn" ).on( "click", function(e) { // if a button is clicked
   e.preventDefault();
-  const buttonClickedClasses = e.target.parentNode.classList.value.split(' '); //
-  buttonClickedHourClass = buttonClickedClasses[1];
 
-  for (let i=0; i<hoursArray.length; i++) { //for loop to remove the relevant css classes
+  //retrieve the class list of the parent of that button
+  const buttonClickedClasses = e.target.parentNode.classList.value.split(' '); 
+  buttonClickedHourClass = buttonClickedClasses[1]; //index 1 = relevant class e.g 'hourNine'
+
+  for (let i=0; i<hoursArray.length; i++) {
     hourArr = hoursArray[i];
     if (hourArr[2] === buttonClickedHourClass) {
-    let scheduledEvent = hourArr[0].children('input').val();
+    let scheduledEvent = hourArr[0].children('input').val(); // obtain the text value of the user input
     //local storage saves key as the hour class and value as the text input
-    localStorage.setItem(hourArr[2], JSON.stringify(scheduledEvent)); 
+    localStorage.setItem(hourArr[2], JSON.stringify(scheduledEvent)); // push the event, with its class to local storage 
   }}
   })
   
 
 
+
+
   // clear all schedule items button
   const clearButton = $('<button class="clearButton rounded m-3 px-5 py-2 " type="button">Clear Entire Schedule</button>');
   
-  $('ol').append(clearButton)
+  $('ol').append(clearButton) // add button to ol
 
   $(clearButton).on('click', function(e){
     e.preventDefault();
-    localStorage.clear();
-    location.reload();
+    localStorage.clear(); //clear the local storage
+    location.reload(); //reload the page
   })
